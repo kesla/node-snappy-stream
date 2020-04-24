a433825576432ca6e7ca76197e10d423bd8a98af
David Björklund <david.bjorklund@gmail.com>
2020-04-24 13:25:00 +0200

    checksum fix in newer node.js

---
+++ test/checksum-test.js
@@ -0,0 +1,43 @@
+var checksum = require("../lib/checksum");
+var fs = require("fs");
+var join = require("path").join;
+var test = require("tap").test;
+
+function bufferToArray(buffer) {
+  var array = new Array(buffer.length);
+  for (var i = 0; i < buffer.length; ++i) {
+    array[i] = buffer[i];
+  }
+  return array;
+}
+
+if ("UPDATE_EXPECTED" in process.env) {
+  var expectedRows = [];
+  for (var i = 0; i < 1000; ++i) {
+    var buffer = new Buffer(1);
+    buffer[0] = i;
+
+    console.log(checksum(buffer));
+
+    expectedRows.push(bufferToArray(checksum(buffer)));
+  }
+
+  fs.writeFileSync(
+    join(__dirname, "checksum.expected"),
+    JSON.stringify(expectedRows)
+  );
+}
+
+var expectedRows = JSON.parse(
+  fs.readFileSync(join(__dirname, "checksum.expected"))
+);
+
+test("Checksum", function (t) {
+  expectedRows.forEach(function (expected, index) {
+    var buffer = new Buffer(1);
+    buffer[0] = index;
+    var actual = bufferToArray(checksum(buffer));
+    t.deepEqual(actual, expected, 'Buffer created from ' + index);
+  });
+  t.end();
+});
